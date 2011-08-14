b385a0d4ad79ba9837570ee64760cdfa411adae0
Tim Gray <tgray@125px.com>
2011-08-14 14:20:44 -0400

    Putmail 1.4.1

--- putmail.rb
+++ putmail.rb
@@ -1,9 +1,13 @@
 require 'formula'
 
-class PutmailPy < Formula
-  url 'http://downloads.sourceforge.net/project/putmail/putmail.py/1.4/putmail.py-1.4.tar.bz2'
-  homepage 'http://putmail.sourceforge.net/home.html'
-  md5 'cb512effdb98731821b09dedcbc641ed'
+class Putmail < Formula
+  url 'https://github.com/tgray/putmail/tarball/v1.4.1'
+  homepage 'https://github.com/tgray/putmail/'
+  head 'git://github.com/tgray/putmail.git'
+  
+  unless ARGV.build_head?
+    md5 '30414407ff040091d55f940df195c6c2'
+  end
 
   def install
     system "export PREFIX=#{prefix};./install.sh"
