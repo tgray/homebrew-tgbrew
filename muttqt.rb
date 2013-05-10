5be94b000ab9e823f6f8af298d8b5efddd275aa8
Tim Gray <tgray@125px.com>
2013-05-10 10:26:31 -0400

    muttqt 0.1.0 conf update

--- muttqt.rb
+++ muttqt.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Muttqt < Formula
   homepage 'https://github.com/tgray/muttqt'
   url 'https://github.com/tgray/muttqt/archive/0.1.0.tar.gz'
-  sha1 '97a1de6b8001e9c4b11386a58e3305b4151b5b34'
+  sha1 '6df69a2e0c3548655e682eee7c848e7271e02b5f'
   version '0.1.0'
 
   def install
