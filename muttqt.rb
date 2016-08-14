4f01aa5458fa4abd8213525cb6915de48e9d7664
alexandru totolici <alexandru@fastly.com>
2016-08-14 16:26:52 -0700

    use sha256 and avoid deprecation warnings

--- muttqt.rb
+++ muttqt.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Muttqt < Formula
   homepage 'https://github.com/tgray/muttqt'
   url 'https://github.com/tgray/muttqt/archive/0.1.0.tar.gz'
-  sha1 '6df69a2e0c3548655e682eee7c848e7271e02b5f'
+  sha256 '2bdd888fa6fb95e0f96e08090595c8c94e0913e9db672aa863cb2da576794fda'
   version '0.1.0'
 
   def install
