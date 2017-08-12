9e222309a5f7273dc2607fd69657a88c15e606b1
Tim Gray <tgray@125px.com>
2017-08-12 15:35:58 -0400

    Updated contacts2 formula
    v0.2.1

--- contacts2.rb
+++ contacts2.rb
@@ -1,15 +1,10 @@
-require 'formula'
-
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Contacts2 < Formula
-  homepage 'https://github.com/tgray/contacts'
-  url 'https://github.com/tgray/contacts/archive/0.2.tar.gz'
-  sha256 '38ce504dc66473f75767c3cae7228f0bfc06c9e2f8b96ed4f9cd99f73a7969d3'
+  desc "A command line interface to the macOS Address Book."
+  homepage "https://github.com/tgray/contacts"
+  url "https://github.com/tgray/contacts/archive/0.2.1.tar.gz"
+  sha256 "2401697690751fa3c796f8b8fe0350f7d7977117cf6027b7ad4e80f5ceed8d5f"
 
-  conflicts_with 'contacts', :because => 'contacts is no longer maintained and this formula replaces it.'
-
+  conflicts_with "contacts", :because => "contacts is no longer maintained and this formula replaces it."
 
   def install
     system "make"
@@ -17,11 +12,6 @@ class Contacts2 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test contacts`.
-    system "contacts -v"
+    system "contacts", "-v"
   end
 end
